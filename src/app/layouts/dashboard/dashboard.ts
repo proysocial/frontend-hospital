ab8a1ab01713487e597c1c00244ca1fcecd104b9
Favio-coder <faviusam@gmail.com>
2026-02-04 08:51:50 -0500

    compatbilidad móvil agregada

--- src/app/layouts/dashboard/dashboard.ts
+++ src/app/layouts/dashboard/dashboard.ts
@@ -1,12 +1,23 @@
 import { ChangeDetectionStrategy, Component , inject} from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from '../../components/sidebar/sidebar';
+import { Device } from '../../services/device';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterOutlet, Sidebar],
+  imports: [CommonModule, RouterOutlet, Sidebar],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Dashboard { }
+export class Dashboard {
+  //Inyección de servicio para detectar si es celular o web 
+  private device = inject(Device)
+
+  isHandset$ = this.device.isHandset$
+  isTablet$ = this.device.isTablet$
+  isDesktop$ = this.device.isDesktop$
+
+
+ }
