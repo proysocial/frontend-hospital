10d44a998aae28a282dc2b794fe65641e7c116cd
Favio-coder <faviusam@gmail.com>
2026-02-06 23:08:15 -0500

    Mejora User-Profile-FAVIO

--- src/app/components/user-profile/user-profile.ts
+++ src/app/components/user-profile/user-profile.ts
@@ -1,42 +1,41 @@
-import { ChangeDetectionStrategy, Component} from '@angular/core';
-import { Cargo } from '../../enums/cargo.enum';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Usuario } from '../../interfaces/Usuario';
 
 @Component({
   selector: 'app-user-profile',
-  imports: [],
+  standalone: true,
   templateUrl: './user-profile.html',
   styleUrl: './user-profile.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserProfile {
-  //Atributos
-  nombre:String = "Jorge Lopez"
-  cargo:Cargo = Cargo.Administrador
 
-  //Modal
-  isOpen: Boolean = false
+  isOpen = false;
 
-  //Getter
-  get iniciales(): String {
-    const partesNombre = this.nombre.split(" "); 
+  usuario: Usuario | null = null;
 
-    const primera = partesNombre[0].charAt(0);
-    const segunda = partesNombre.length > 1 ? partesNombre[1].charAt(0) : "";
-
-    return (primera + segunda).toUpperCase();
+  constructor() {
+    const raw = localStorage.getItem('usuario');
+    this.usuario = raw ? JSON.parse(raw) : null;
   }
 
-  //Funciones para el modal 
+  get iniciales(): string {
+    if (!this.usuario) return '';
+
+    const primerNombre = this.usuario.nombre.trim().split(' ')[0];
+    const primerApellido = this.usuario.apellido.trim().split(' ')[0];
+
+    return (
+      primerNombre.charAt(0) + primerApellido.charAt(0)
+    ).toUpperCase();
+  }
+
   abrirModal(event: MouseEvent) {
-    event.stopPropagation()
-    this.isOpen = true
+    event.stopPropagation();
+    this.isOpen = true;
   }
 
   cerrarModal() {
-    this.isOpen = false
+    this.isOpen = false;
   }
-
 }
-
-
-
